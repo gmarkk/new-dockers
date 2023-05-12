b18aad8494457f0a028827aa0a7f38543eff8085
root <root@ip-172-31-17-214.ec2.internal>
2023-05-12 08:08:15 +0000

    Added ElasticBeanStalk

--- server/index.js
+++ server/index.js
@@ -1,85 +1,72 @@
-const keys = require('./keys');
+const keys = require("./keys");
 
-// Express App set
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+// Express App Setup
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Postgres client set
-const { Pool } = require('pg');
+// Postgres Client Setup
+const { Pool } = require("pg");
 const pgClient = new Pool({
   user: keys.pgUser,
   host: keys.pgHost,
   database: keys.pgDatabase,
   password: keys.pgPassword,
-  port: keys.pgPort
+  port: keys.pgPort,
 });
 
-pgClient.on('connect', (client) => {
-  client.query('CREATE TABLE IF NOT EXISTS values (number INT)')
+pgClient.on("connect", (client) => {
+  client
+    .query("CREATE TABLE IF NOT EXISTS values (number INT)")
     .catch((err) => console.error(err));
 });
 
 // Redis Client Setup
-const redis = require('redis');
+const redis = require("redis");
 const redisClient = redis.createClient({
   host: keys.redisHost,
   port: keys.redisPort,
-  retry_strategy: () => 1000
+  retry_strategy: () => 1000,
 });
 const redisPublisher = redisClient.duplicate();
 
 // Express route handlers
-app.get('/', (req, res) => {
-  res.send('Hi');
+
+app.get("/", (req, res) => {
+  res.send("Hi");
 });
 
-app.get('/values/all', async (req, res) => {
-  const values = await pgClient.query('SELECT * from values');
+app.get("/values/all", async (req, res) => {
+  const values = await pgClient.query("SELECT * from values");
+
   res.send(values.rows);
 });
 
-app.get('/values/current', async (req, res) => {
-  redisClient.hgetall('values', (err, values) => {
+app.get("/values/current", async (req, res) => {
+  redisClient.hgetall("values", (err, values) => {
     res.send(values);
   });
 });
 
-app.post('/values', async (req, res) => {
+app.post("/values", async (req, res) => {
   const index = req.body.index;
+
   if (parseInt(index) > 40) {
-    return res.status(422).send('Index too high');
+    return res.status(422).send("Index too high");
   }
-  redisClient.hset('values', index, 'Nothing yet');
-  redisPublisher.publish('insert', index);
-  pgClient.query('INSERT INTO values(number) VALUES ($1)', [index], (err, result) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Error inserting into database');
-    }
-    res.send({ working: true });
-  });
+
+  redisClient.hset("values", index, "Nothing yet!");
+  redisPublisher.publish("insert", index);
+  pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
+
+  res.send({ working: true });
 });
 
-// Redis message handler
-redisClient.on('message', (channel, message) => {
-  const index = parseInt(message);
-  redisClient.hset('values', index, fibonacci(index));
+app.listen(5000, (err) => {
+  console.log("Listening");
 });
 
-redisClient.subscribe('insert');
-
-// Fibonacci calculation function
-function fibonacci(num) {
-  if (num === 0) return 0;
-  if (num === 1) return 1;
-  return fibonacci(num - 1) + fibonacci(num - 2);
-}
-
-app.listen(5000, err => {
-  console.log('Listening');
-});
